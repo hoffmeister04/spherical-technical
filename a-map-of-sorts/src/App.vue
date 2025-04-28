3b90f6eedc78290ccbe758ec4c0c12bc145a0e2c
hoffmeister <anhoffman2323@gmail.com>
2025-04-28 14:49:55 -0400

    basic pins/descriptions functioning

--- a-map-of-sorts/src/App.vue
+++ a-map-of-sorts/src/App.vue
@@ -7,54 +7,47 @@ import mapboxgl from 'mapbox-gl'
 onMounted(() => {
   mapboxgl.accessToken = import.meta.env.VITE_TOKEN
 
+  // initial map
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v12',
     center: [-82.879143, 40.021568],   // gahanna, ohio
     zoom: 12
   })
+
+  // add a pin with description to the map
+  map.on('click', (e) => {
+    const pin = e.lngLat
+    const description = prompt('Add a description for your pin:')
+
+    if (description) {
+      const marker = new mapboxgl.Marker()
+        .setLngLat([pin.lng, pin.lat])
+        .setPopup(new mapboxgl.Popup().setText(description))
+        .addTo(map)
+    }
+  })
 })
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <div id="app">
     <div id="map" class="map-container"></div>
   </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+html, body, #app, .map-container {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.map-container {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%
 }
 </style>
