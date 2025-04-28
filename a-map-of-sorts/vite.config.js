34d9620b8ae833ae95a775c7c83997c7884840ab
hoffmeister <anhoffman2323@gmail.com>
2025-04-28 18:55:49 -0400

    adjusted base location

--- a-map-of-sorts/vite.config.js
+++ a-map-of-sorts/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/spherical-technical/',
+  base: '/spherical-technical/a-map-of-sorts/',
   plugins: [
     vue(),
     vueDevTools(),
